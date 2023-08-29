3e3841b3ceec615eb0132f433542de215ccf0ce3
packituz <pacohm94@gmail.com>
2023-08-29 16:14:47 -0600

    refactor(ansible): modified playbooks configurations to play better with vagrant and production machines
    configured playbooks and makefile to enable seamlessly integration of staging and production environment

---
+++ scripts/make/run_staging.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Executes main playbook against vagrant machine. Any additional flags for ansible-playbook
+# can be passed using the PARAMS variable.
+# --tags flag must be passed in order to execute the desired steps.
+
+PARAMS="${1}"
+cd playbooks && ANSIBLE_ARGS="-e secrets_file=./roles/server_setup/vars/secrets_staging.yml ${PARAMS}" vagrant up --provision
