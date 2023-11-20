2854837b0fc786ab7db27a85c89b8715d8ac5892
packituz <pacohm94@gmail.com>
2023-11-20 14:51:35 -0600

    refactor(media): modified directory where files get saved
    added mergerfs to merge multiple disk as one

--- scripts/make/run_staging.sh
+++ scripts/make/run_staging.sh
@@ -4,4 +4,4 @@
 # --tags flag must be passed in order to execute the desired steps.
 
 PARAMS="${1}"
-cd playbooks && ANSIBLE_ARGS="-e secrets_file=./roles/server_setup/vars/secrets_staging.yml ${PARAMS}" vagrant up --provision
+cd playbooks && ANSIBLE_ARGS="-e secrets_file=./roles/server_setup/vars/secrets_staging.yml ${PARAMS}" VAGRANT_EXPERIMENTAL=disks vagrant up --provision
