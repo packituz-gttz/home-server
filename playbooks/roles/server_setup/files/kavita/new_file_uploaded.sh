ebc000067bc909aa16b8985351a40ff7ef6490a0
packituz <pacohm94@gmail.com>
2024-03-11 21:36:47 -0600

    feat(kavita): added cron job to automatically create directories for kavita uploads
    this script creates the required structure for kavita to recognize a book

---
+++ playbooks/roles/server_setup/files/kavita/new_file_uploaded.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Starting Kavita Directory Creation for new files"
+
+PROGRAMING_BOOKS_PATH=/mnt/media/Kavita/Books/Programming
+NOVELS_BOOKS_PATH=/mnt/media/Kavita/Books/Novels
+
+cd "${PROGRAMING_BOOKS_PATH}" || exit 1
+
+find "${PROGRAMING_BOOKS_PATH}" -maxdepth 1 -type f -exec sh -c 'mkdir "$(basename "$1" .pdf)"; mv "$1" "$(basename "$1" .pdf)"' sh {} \;
+find "${PROGRAMING_BOOKS_PATH}" -maxdepth 1 -type f -exec sh -c 'mkdir "$(basename "$1" .epub)"; mv "$1" "$(basename "$1" .epub)"' sh {} \;
+
+cd "${NOVELS_BOOKS_PATH}" || exit 1
+
+find "${NOVELS_BOOKS_PATH}" -maxdepth 1 -type f -exec sh -c 'mkdir "$(basename "$1" .pdf)"; mv "$1" "$(basename "$1" .pdf)"' sh {} \;
+find "${NOVELS_BOOKS_PATH}" -maxdepth 1 -type f -exec sh -c 'mkdir "$(basename "$1" .epub)"; mv "$1" "$(basename "$1" .epub)"' sh {} \;
+
+echo "Finished Kavita Directory Creation for new files"
