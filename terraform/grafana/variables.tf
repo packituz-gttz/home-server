cd2d16d6a62b6efbd635f5679a0ea8131c781032
packituz <pacohm94@gmail.com>
2024-09-12 09:42:28 -0600

    feat(grafana): migrated grafana to docker and using terraform to manage it
    managing grafana alerts, dashboards, users and data sources with terraform

---
+++ terraform/grafana/variables.tf
@@ -0,0 +1,46 @@
+variable "admin_user" {
+  type        = string
+  description = "Grafana admin user name"
+}
+
+variable "admin_email" {
+  type        = string
+  description = "Grafana admin user email"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Grafana admin user password"
+}
+
+variable "main_organization_name" {
+  type        = string
+  description = "Main organization name"
+  default     = "Main Org."
+}
+
+variable "host_url" {
+  type        = string
+  description = "Host URL"
+}
+
+variable "prometheus_data_source_url" {
+  type        = string
+  description = "Prometheus data source URL"
+  default     = "http://localhost:9090"
+}
+
+variable "prometheus_data_source_uid" {
+  type        = string
+  description = "Prometheus data source UID"
+}
+
+variable "telegram_bot_token" {
+  type        = string
+  description = "Telegram bot token"
+}
+
+variable "telegram_bot_chat_id" {
+  type        = string
+  description = "Telegram bot chat ID"
+}
