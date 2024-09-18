592297d03c164e8ba420cb5bd128c39aa533ff35
packituz <pacohm94@gmail.com>
2024-09-18 11:30:30 -0600

    ci(terraform): testing why cloudflare access application is not working

--- terraform/cloudflare/cloudflare_records.tf
+++ terraform/cloudflare/cloudflare_records.tf
@@ -38,6 +38,7 @@ resource "cloudflare_zero_trust_access_application" "depas_app" {
   name             = "Access application for ${module.records.records_info["depas"].hostname}"
   domain           = module.records.records_info["depas"].hostname
   session_duration = "15m"
+  type             = "self_hosted"
 
   policies = [
     cloudflare_zero_trust_access_policy.depas_policy.id
@@ -45,9 +46,10 @@ resource "cloudflare_zero_trust_access_application" "depas_app" {
 }
 
 resource "cloudflare_zero_trust_access_policy" "depas_policy" {
-  decision = "allow"
-  name     = "Policy for accessing ${module.records.records_info["depas"].hostname}"
-  zone_id  = var.cloudflare_zone_id
+  decision         = "allow"
+  name             = "Policy for accessing ${module.records.records_info["depas"].hostname}"
+  zone_id          = var.cloudflare_zone_id
+  session_duration = "15m"
   include {
     email = var.cloudflare_admin_emails
   }
