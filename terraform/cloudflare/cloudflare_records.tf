30699efb67c6bb8843eeb513ed4837cc695f5a3c
packituz <pacohm94@gmail.com>
2024-09-18 11:23:28 -0600

    ci(terraform): updating to new access_application and policy resources version

--- terraform/cloudflare/cloudflare_records.tf
+++ terraform/cloudflare/cloudflare_records.tf
@@ -33,20 +33,21 @@ module "records" {
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_access_application" "depas_app" {
+resource "cloudflare_zero_trust_access_application" "depas_app" {
   zone_id          = var.cloudflare_zone_id
   name             = "Access application for ${module.records.records_info["depas"].hostname}"
   domain           = module.records.records_info["depas"].hostname
   session_duration = "15m"
+
+  policies = [
+    cloudflare_zero_trust_access_policy.depas_policy.id
+  ]
 }
 
-resource "cloudflare_access_policy" "depas_policy" {
-  application_id   = cloudflare_access_application.depas_app.id
-  decision         = "allow"
-  name             = "Policy for accessing ${module.records.records_info["depas"].hostname}"
-  precedence       = 1
-  zone_id          = var.cloudflare_zone_id
-  session_duration = "15m"
+resource "cloudflare_zero_trust_access_policy" "depas_policy" {
+  decision = "allow"
+  name     = "Policy for accessing ${module.records.records_info["depas"].hostname}"
+  zone_id  = var.cloudflare_zone_id
   include {
     email = var.cloudflare_admin_emails
   }
