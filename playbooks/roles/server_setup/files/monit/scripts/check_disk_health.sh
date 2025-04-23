500c6b0bf98aa46ce4f266c83b37d59707cc72ff
packituz <pacohm94@gmail.com>
2025-04-23 16:58:13 -0600

    feat(monit): updated monit scripts
    removed disk checks since grafana takes care of that, added host checks for lychee and grafana

--- playbooks/roles/server_setup/files/monit/scripts/check_disk_health.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-WEAR_DOWN=$(nvme smart-log /dev/nvme0n1 --o json | jq '.percent_used')
-if [ "${WEAR_DOWN}" -gt 45 ]; then
-  echo "DISK WEAR DOWN ${WEAR_DOWN}"
-  exit 250
-fi
-exit 0
