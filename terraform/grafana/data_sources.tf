acb4ebfa0a025e4749adc78679967a17ed6d6bbd
packituz <pacohm94@gmail.com>
2025-05-23 12:22:11 -0600

    feat(loki): added loki and promtail to get logs from containers
    only a handful of containers were integrated to loki, these being the most importants

--- terraform/grafana/data_sources.tf
+++ terraform/grafana/data_sources.tf
@@ -10,3 +10,11 @@ resource "grafana_data_source" "prometheus" {
   uid = var.prometheus_data_source_uid
 
 }
+
+resource "grafana_data_source" "loki" {
+  name = "Loki"
+  type = "loki"
+  url = var.loki_data_source_url
+
+  uid = var.loki_data_source_uid
+}
