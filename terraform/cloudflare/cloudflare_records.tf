56932039361be8e4f23d32affbd6e1933777e157
packituz <pacohm94@gmail.com>
2025-09-28 09:55:44 -0600

    feat(cloudflare): enabled app protection policy for docuseal and lychee external access

--- terraform/cloudflare/cloudflare_records.tf
+++ terraform/cloudflare/cloudflare_records.tf
@@ -112,3 +112,68 @@ resource "cloudflare_zero_trust_access_application" "cars_app" {
   ]
 
 }
+
+resource "cloudflare_zero_trust_access_policy" "photos_policy" {
+  decision = "allow"
+  name     = "Policy for accessing photos"
+  include = [
+    {
+      email = {
+        email = var.admin_dg_email
+      }
+    },
+    {
+      email = {
+        email = var.cloudflare_notification_email
+      }
+    }
+  ]
+  account_id = var.cloudflare_account_id
+}
+
+resource "cloudflare_zero_trust_access_application" "photos_app" {
+  zone_id          = var.cloudflare_zone_id
+  type             = "self_hosted"
+  name             = "Access application for photos"
+  domain           = "photos.packituz.dev"
+  session_duration = "60m"
+  policies = [
+    {
+      id         = cloudflare_zero_trust_access_policy.photos_policy.id
+      precedence = 1
+    }
+  ]
+
+}
+resource "cloudflare_zero_trust_access_policy" "docs_policy" {
+  decision = "allow"
+  name     = "Policy for accessing docs"
+  include = [
+    {
+      email = {
+        email = var.admin_dg_email
+      }
+    },
+    {
+      email = {
+        email = var.cloudflare_notification_email
+      }
+    }
+  ]
+  account_id = var.cloudflare_account_id
+}
+
+resource "cloudflare_zero_trust_access_application" "docs_app" {
+  zone_id          = var.cloudflare_zone_id
+  type             = "self_hosted"
+  name             = "Access application for docs"
+  domain           = "docs.packituz.dev"
+  session_duration = "60m"
+  policies = [
+    {
+      id         = cloudflare_zero_trust_access_policy.docs_policy.id
+      precedence = 1
+    }
+  ]
+
+}
