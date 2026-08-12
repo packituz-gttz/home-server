699e126c2a7853dc3b1c587144c613ca0e10f94a
packituz-gttz <pacohm94@gmail.com>
2026-08-12 16:48:52 -0600

    fix: terraformush

--- terraform/modules/cloudflare/records/terraform.tf
+++ terraform/modules/cloudflare/records/terraform.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.0"
+      version = "~> 4.49.0"
     }
     random = {
       source  = "hashicorp/random"
