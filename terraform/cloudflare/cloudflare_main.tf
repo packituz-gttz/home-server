378b10a3a43470afdbc45a3efdcb49a4a4733ed2
packituz-gttz <pacohm94@gmail.com>
2026-08-12 18:30:00 -0600

    fix: terraform

--- terraform/cloudflare/cloudflare_main.tf
+++ terraform/cloudflare/cloudflare_main.tf
@@ -1,24 +1,24 @@
-resource "random_password" "tunnel_secret" {
-  length = 64
-}
-
-resource "cloudflare_zero_trust_tunnel_cloudflared" "packituz_dev_tunnel" {
-  account_id    = var.cloudflare_account_id
-  name          = "packituz-dev-tunnel"
-  tunnel_secret = base64sha256(random_password.tunnel_secret.result)
-  config_src    = "cloudflare"
-}
-
-
-resource "cloudflare_page_rule" "packituz_dev_cache_rule" {
-  zone_id = var.cloudflare_zone_id
-
-  status   = "active"
-  target   = "*.${var.domain}/"
-  priority = 1
-
-  actions = {
-    cache_level         = "bypass"
-    disable_performance = true
-  }
-}
+# resource "random_password" "tunnel_secret" {
+#   length = 64
+# }
+#
+# resource "cloudflare_zero_trust_tunnel_cloudflared" "packituz_dev_tunnel" {
+#   account_id    = var.cloudflare_account_id
+#   name          = "packituz-dev-tunnel"
+#   tunnel_secret = base64sha256(random_password.tunnel_secret.result)
+#   config_src    = "cloudflare"
+# }
+#
+#
+# resource "cloudflare_page_rule" "packituz_dev_cache_rule" {
+#   zone_id = var.cloudflare_zone_id
+#
+#   status   = "active"
+#   target   = "*.${var.domain}/"
+#   priority = 1
+#
+#   actions = {
+#     cache_level         = "bypass"
+#     disable_performance = true
+#   }
+# }
