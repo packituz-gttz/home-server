1e9d9aebbf2382405d63bc90e1709089184b5e90
packituz-gttz <pacohm94@gmail.com>
2026-08-14 20:23:39 -0600

    feat: grafana update

--- terraform/grafana/dashboards.tf
+++ terraform/grafana/dashboards.tf
@@ -1,5 +1,8 @@
-resource "grafana_dashboard" "main" {
-  config_json = file("${path.module}/dashboards_files/grafana-main-dashboard.json")
+resource "grafana_apps_dashboard_dashboard_v2" "main" {
+  spec {
+    json = file("${path.module}/dashboards_files/grafana-main-dashboard.json")
+  }
+
 }
 
 resource "grafana_dashboard" "docker" {
