9601e19cccb9e93cd1f9a34336738135a26a2b06
packituz-gttz <pacohm94@gmail.com>
2026-08-23 19:12:05 -0600

    fix: disk read latency alert when no data

--- terraform/grafana/alerts.tf
+++ terraform/grafana/alerts.tf
@@ -454,8 +454,8 @@ resource "grafana_rule_group" "system_alerts" {
     name           = "Disk Read Latency"
     for            = "5m"
     condition      = "B"
-    no_data_state  = "Normal"
-    exec_err_state = "KeepLast"
+    no_data_state  = "OK"
+    exec_err_state = "OK"
     annotations = {
       "summary" : "Disk Read Latency Issue",
       "description" : "Disk Read Latency Issue"
